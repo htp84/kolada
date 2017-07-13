e56af3e6aed9715a990da81339aa41ed762aea24
htp84 <henric.sundberg@gmail.com>
2017-07-13 14:13:04 +0200

    added helper module

---
+++ kolada_api/_json/structure.py
@@ -0,0 +1,15 @@
+
+def _metadata(group):
+    return  (group['id'],
+    group['prel_publication_date'],
+    group['municipality_type'],
+    group['publ_period'],
+    group['operating_area'],
+    group['auspices'],
+    group['publication_date'],
+    group['perspective'],
+    group['is_divided_by_gender'],
+    group['ou_publication_date'],
+    (group['has_ou_data']),
+    group['title'],
+    group['description'])
