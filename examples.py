b87cab46227a0f88b53aa00bff1d2edbafe87cf0
htp84 <henric.sundberg@gmail.com>
2017-07-13 14:13:17 +0200

    added examples

--- examples.py
+++ examples.py
@@ -1,5 +1,15 @@
-import kolada
+'''
+A file with examples
+'''
+from  kolada_api import Kpi, Municipality
 
-metadata = kolada.Kpi.metadata()
-kpi = kolada.Kpi.kpi()
-print(kpi)
+#METADATA = Kpi.metadata()
+#KPI = Kpi.kpi()
+METADATA_SEARCH = Kpi.metadata_search('räddning', 'K', 'operating_area')
+#MUNICIPALITY_GROUP_MEMBERS = Municipality.group_members()
+
+if __name__ == '__main__':
+    #print(METADATA)
+    #print(KPI)
+    print(METADATA_SEARCH)
+    #print(MUNICIPALITY_GROUP_MEMBERS)
