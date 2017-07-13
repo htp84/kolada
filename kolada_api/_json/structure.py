f0932c269d05e7e40e7b49f0447f90f926e2d548
htp84 <henric.sundberg@gmail.com>
2017-07-13 21:17:48 +0200

    added functions

--- kolada_api/_json/structure.py
+++ kolada_api/_json/structure.py
@@ -1,15 +1,26 @@
 
 def _metadata(group):
     return  (group['id'],
-    group['prel_publication_date'],
-    group['municipality_type'],
-    group['publ_period'],
-    group['operating_area'],
-    group['auspices'],
-    group['publication_date'],
-    group['perspective'],
-    group['is_divided_by_gender'],
-    group['ou_publication_date'],
-    (group['has_ou_data']),
-    group['title'],
-    group['description'])
+             group['prel_publication_date'],
+             group['municipality_type'],
+             group['publ_period'],
+             group['operating_area'],
+             group['auspices'],
+             group['publication_date'],
+             group['perspective'],
+             group['is_divided_by_gender'],
+             group['ou_publication_date'],
+             str(group['has_ou_data']),
+             group['title'],
+             group['description'])
+
+def _id_title(group):
+    return (group['id'],
+            group['title'])
+
+def _data(group, member):
+    return (group['kpi'],
+            group['municipality'],
+            group['period'],
+            member['value'],
+            member['gender'])
