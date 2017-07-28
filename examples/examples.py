2184218bc76290839e91a2bea9543c406139029c
htp84 <henric.sundberg@gmail.com>
2017-07-28 23:13:50 +0200

    changed example

--- examples/examples.py
+++ examples/examples.py
@@ -4,7 +4,7 @@ A file with examples
 from kolada import Kpi, Municipality
 
 #METADATA = Kpi.metadata()
-KPI = Kpi.kpi(filter_kpis='', id_or_name='name', generator='genesator')
+KPI = Kpi.kpi(filter_kpis='', id_or_name='name')
 #KPI_AS_LIST = Kpi.kpi(inner_type='list')
 #METADATA_SEARCH = Kpi.metadata_search('räddning', 'K', 'operating_area')
 #METADATA_SEARCH_AS_LIST = Kpi.metadata_search('räddning', 'K', 'operating_area', 'yes')
@@ -13,6 +13,7 @@ KPI = Kpi.kpi(filter_kpis='', id_or_name='name', generator='genesator')
 #DATA_KPI = Kpi.data_per_municipality('N00002', '0860')
 #data_m = Municipality.data_per_year('0860','2016')
 #data_k = Kpi.data_per_year('N00002', '2016')
+
 
 if __name__ == '__main__':
     #print(METADATA)
