e7b7cd1ed4a15a82acfeec77982e02e6d5698086
htp84 <henric.sundberg@gmail.com>
2017-08-18 21:22:14 +0200

    added columns

--- kolada/_json/structure.py
+++ kolada/_json/structure.py
@@ -12,17 +12,34 @@ def _metadata(group):
              str(group['ou_publication_date']),
              str(group['has_ou_data']),
              str(group['title']),
-             str(group['description']))             
+             str(group['description']))
+       
+COLUMNS_METADATA = ['id',
+                    'prel_publication_date',
+                    'municipality_type',
+                    'publ_period',
+                    'operating_area',
+                    'auspices',
+                    'publication_date',
+                    'perspective',
+                    'is_divided_by_gender',
+                    'ou_publication_date',
+                    'has_ou_data',
+                    'title',
+                    'description']
 
 def _id_title(group, inner_type):
     if inner_type.lower() == 'tuple':
-        return (group['id'],
-                group['title'])
+        return (str(group['id']),
+                str(group['title']))
     elif inner_type.lower() == 'list':
-        return [group['id'],
-                group['title']]
+        return [str(group['id']),
+                str(group['title'])]
     else:
         raise KeyError('inner_type can only be \'tuple\' or \'list\'.')
+
+COLUMNS_ID_TITLE = ['id',
+                    'title']
 
 def _data(group, member):
     return (group['kpi'],
@@ -30,6 +47,12 @@ def _data(group, member):
             group['period'],
             member['value'],
             member['gender'])
+
+COLUMNS_DATA = ['kpi',
+                'municipality',
+                'period',
+                'value',
+                'gender']
 
 def _ou(group):
     return (group['id'],
