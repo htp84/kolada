0b018ee556923a80a979ecfbb852ef3a18d207da
htp84 <henric.sundberg@gmail.com>
2017-10-21 16:49:16 +0200

    changed from unittest to pytest

--- tests/test_kolada_api.py
+++ tests/test_kolada_api.py
@@ -4,47 +4,39 @@
 """Tests for `kolada_api` package."""
 
 
-import unittest
-
-#from kolada import Kpi, Municipality
 from context import kolada
+import pytest
 
-class TestPython_kolada_api(unittest.TestCase):
-    """Tests for `python_boilerplate` package."""
 
-    def Kpi_type_000(self):
-        """Test something."""
-        with self.assertRaises(TypeError):
-            kolada.Kpi.kpi(filter_kpis=1)
+def test_Kpi_type_000():
+    """Test something."""
+    with pytest.raises(TypeError):
+        kolada.Kpi.kpi(filter_kpis=1)
 
-        
-    def Kpi_Key_000(self):
-        """Test something."""
-        with self.assertRaises(KeyError):
-            kolada.Kpi.kpi(filter_kpis='s')
     
-    
-    def Kpi_instance_tuple(self):
-        """Test something."""
-        self.assertIsInstance(kolada.Kpi.kpi()[0][0], tuple)
-    
-    
-    def Kpi_instance_list(self):
-        """Test something."""
-        self.assertIsInstance(kolada.Kpi.kpi(inner_type='list')[0][0], list)
-    
-    def Kpi_key_001(self):
-        """Test something."""
-        with self.assertRaises(KeyError):
-            kolada.Kpi.kpi(inner_type='str')
-    
+def test_Kpi_Key_000():
+    """Test something."""
+    with pytest.raises(KeyError):
+        kolada.Kpi.kpi(filter_kpis='s')
 
-    def Kpi_type_001(self):
-        """Test something."""
-        with self.assertRaises(TypeError):
-            kolada.Kpi.kpi(inner_type=1)
 
-            
+def test_Kpi_instance_tuple():
+    """Test something."""
+    assert isinstance(kolada.Kpi.kpi()[0], tuple)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_Kpi_instance_list():
+    """Test something."""
+    assert isinstance(kolada.Kpi.kpi(inner_type='list')[0], list)
+
+def test_Kpi_key_001():
+    """Test something."""
+    with pytest.raises(KeyError):
+        kolada.Kpi.kpi(inner_type='str')
+
+
+def test_Kpi_type_001():
+    """Test something."""
+    with pytest.raises(TypeError):
+        kolada.Kpi.kpi(inner_type=1)
+
